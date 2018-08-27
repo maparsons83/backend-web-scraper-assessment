4d5b61b21838b3f3daddd77446028cfabc69c0c7
Matt Parsons <maparsons83@yahoo.com>
2018-08-27 14:36:06 -0400

    added scraping for relative urls

--- scraper.py
+++ scraper.py
@@ -3,9 +3,10 @@ import re
 import requests
 import sys
 import pprint
+from bs4 import BeautifulSoup
 
 url_search = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]\
-            |[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+            |[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+;\?'
 
 email_search = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
 
@@ -16,7 +17,18 @@ def url_scraper(url):
     url_match = re.findall(url_search, r.text)
     email_match = re.findall(email_search, r.text)
     phone_match = re.findall(r'[\(]?\d{3}[\-, \)]?\d{3}[\-, \s]+\d{4}', r.text)
-    pp.pprint(url_match + email_match + phone_match)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    rel_links = []
+    for rel_url in soup.find_all():
+        rel_href = rel_url.get('href')
+        rel_img = rel_url.get('img')
+        if rel_href:
+            rel_links.append(rel_href)
+        elif rel_img:
+            rel_links.append(rel_img)
+    merged_lists = set(url_match + rel_links)
+    pp.pprint(merged_lists)
+    pp.pprint(email_match + phone_match)
 
 
 def create_parser():
